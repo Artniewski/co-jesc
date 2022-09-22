a4d3a42c81d607a526bfaff17f114fe4a3813b0f
aarczynski <adam.arczynski@gmail.com>
2022-09-22 12:00:24 +0200

    cache

---
+++ backend-java/src/main/java/com/ocado/cojesc/service/CacheAwareFacebookRestaurantService.java
@@ -0,0 +1,35 @@
+package com.ocado.cojesc.service;
+
+import com.ocado.cojesc.client.ScraperFeignClient;
+import com.ocado.cojesc.restaurant.Restaurant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CacheAwareFacebookRestaurantService {
+
+    private static final String CACHE_NAME = "lunch-menu";
+
+    private final ScraperFeignClient client;
+
+    @Cacheable(cacheNames = { CACHE_NAME }, key = "#restaurant.name", unless = "#result == null")
+    public List<String> getAllPosts(Restaurant restaurant) {
+        log.info("Menu for {} not found in cache. Searching for new menu.", restaurant.getName());
+        return client.getPosts(restaurant.getFacebookId());
+    }
+
+    @Scheduled(cron = "${cojesc.cache.eviction}")
+    @CacheEvict(cacheNames = { CACHE_NAME }, allEntries = true)
+    public void clearCache() {
+        log.info("{} cache evicted", CACHE_NAME);
+    }
+}
--- backend-java/src/main/java/com/ocado/cojesc/service/FacebookRestaurantsService.java
+++ backend-java/src/main/java/com/ocado/cojesc/service/FacebookRestaurantsService.java
@@ -3,6 +3,7 @@ package com.ocado.cojesc.service;
 import java.util.Collection;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.ocado.cojesc.client.ScraperFeignClient;
@@ -10,18 +11,18 @@ import com.ocado.cojesc.restaurant.Restaurant;
 
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FacebookRestaurantsService {
 
-    private final ScraperFeignClient client;
+    private final CacheAwareFacebookRestaurantService restaurantService;
 
     public List<String> getAllPosts(List<Restaurant> restaurants) {
+        log.info("Searching for all menus.");
         return restaurants.stream()
-                .map(Restaurant::getFacebookId)
-                .map(client::getPosts)
+                .map(restaurantService::getAllPosts)
                 .flatMap(Collection::stream)
                 .toList();
     }
-
 }

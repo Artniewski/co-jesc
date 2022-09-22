c67bc832d11de5abb801a1f0a80aecae98420948
Lena Jasińska <lenajas0110@gmail.com>
2022-09-22 14:31:02 +0200

    Enable parsing relative date format

--- backend-java/src/main/java/com/ocado/cojesc/parser/FacebookPost.java
+++ backend-java/src/main/java/com/ocado/cojesc/parser/FacebookPost.java
@@ -1,14 +1,15 @@
 package com.ocado.cojesc.parser;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public record FacebookPost(String facebookId, String restaurantName, String date, String content) {
+public record FacebookPost(String facebookId, String restaurantName, LocalDate date, String content) {
     public static FacebookPost parse(String facebookId, String facebookPost) {
         String[] facebookPostLines = facebookPost.split("\n");
         try {
             String restaurantName = facebookPostLines[0];
-            String date = facebookPostLines[1];
+            LocalDate date = DateParser.parse(facebookPostLines[1]);
             String content = Arrays.stream(facebookPostLines).skip(2).collect(Collectors.joining("\n"));
             return new FacebookPost(facebookId, restaurantName, date, content);
         } catch (NullPointerException e) {

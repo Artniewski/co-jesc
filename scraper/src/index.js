c79f0db3519cf2a1397bd061029c0d56963f32cc
artniewski <artniewski@gmail.com>
2022-09-27 10:49:56 +0200

    Add mocks for different restaurants

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const scraper = require('./scraper.js')
-const mockData = require('./example_result.json')
+// const mockData = require('./example_result.json')
 
 const app = express();
 const PORT = process.env.PORT || 2137;
@@ -13,6 +13,8 @@ app.get('/', (req, res) => {
 
 
 app.get("/:fbId/mock", async (req, res) => {
+    let fbId = req.params.fbId;
+    const mockData = require('./mocks/' + fbId + '.json')
     res.send(mockData);
 })
 

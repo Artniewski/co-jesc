0d20a6f73ca6c12f0fc14a45b900b24cdf2dae90
aarczynski <adam.arczynski@gmail.com>
2022-10-03 01:21:20 +0200

    fix restaurants time valdity for post from midweek

--- backend-java/src/main/java/com/ocado/cojesc/validator/DateValidator.java
+++ backend-java/src/main/java/com/ocado/cojesc/validator/DateValidator.java
@@ -3,19 +3,21 @@ package com.ocado.cojesc.validator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAdjusters;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 @AllArgsConstructor
 public class DateValidator {
 
     public boolean checkDate(int menuDuration, LocalDate date) {
+        if (menuDuration == 7) {
+            return LocalDate.ofInstant(Instant.now().truncatedTo(ChronoUnit.WEEKS), ZoneId.systemDefault()).isAfter(LocalDate.now());
+        } else if (menuDuration == 1) {
+            return LocalDate.ofInstant(Instant.now().truncatedTo(ChronoUnit.DAYS), ZoneId.systemDefault()).isAfter(LocalDate.now());
+        }
         return date.plusDays(menuDuration).isAfter(LocalDate.now());
     }
 }
